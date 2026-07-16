a5bcfc2bb033e81cfcb9dab9a8a3c25e2b4ded01
Lana Harriet <lanaharriet1408@gmail.com>
2026-07-16 16:11:02 +0530

    Create 577-EmployeeBonus

---
+++ 577-EmployeeBonus/577-EmployeeBonus.sql
@@ -0,0 +1,5 @@
+-- Last updated: 7/16/2026, 4:11:01 PM
+# Write your MySQL query statement below
+SELECT Employee.name,Bonus.bonus FROM Employee 
+LEFT JOIN Bonus ON Employee.empID = Bonus.empID
+WHERE bonus < 1000 OR Bonus IS NULL ;
