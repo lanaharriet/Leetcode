57d9267fb066817c23feb315c59176db0f997931
Lana Harriet <lanaharriet1408@gmail.com>
2026-07-16 16:12:23 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/16/2026, 4:12:22 PM
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT num,
+           LEAD(num, 1) OVER (ORDER BY id) AS next_num,
+           LEAD(num, 2) OVER (ORDER BY id) AS next_next_num
+    FROM Logs
+) t
+WHERE num = next_num AND num = next_next_num;
