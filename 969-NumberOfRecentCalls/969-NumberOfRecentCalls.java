4e17a80b2c9f8e7a54d21d9b45d0170ab9e2a05e
Lana Harriet <lanaharriet1408@gmail.com>
2026-07-16 16:10:10 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.java
@@ -0,0 +1,19 @@
+// Last updated: 7/16/2026, 4:10:08 PM
+class RecentCounter {
+    private static final int[] records = new int[10000]; 
+    private int start;
+    private int end;
+
+    public RecentCounter() {        
+        start = 0;
+        end = 0;
+    }
+    
+    public int ping(int t) {
+        while (start < end && (t - records[start] > 3000)) { 
+            start++; 
+        }
+        records[end++] = t; 
+        return end - start; 
+    }
+}
