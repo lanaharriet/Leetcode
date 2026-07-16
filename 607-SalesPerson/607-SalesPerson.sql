3f2b9556bcf69932501984b63b482dd2d022c107
Lana Harriet <lanaharriet1408@gmail.com>
2026-07-16 16:10:43 +0530

    Create 607-SalesPerson

---
+++ 607-SalesPerson/607-SalesPerson.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/16/2026, 4:10:42 PM
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
