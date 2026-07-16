9fad734df63abd75c4289d93dacd86b532780735
Lana Harriet <lanaharriet1408@gmail.com>
2026-07-16 16:12:44 +0530

    Create 139-WordBreak

---
+++ 139-WordBreak/139-WordBreak.java
@@ -0,0 +1,18 @@
+// Last updated: 7/16/2026, 4:12:43 PM
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (String w : wordDict) {
+                int start = i - w.length();
+                if (start >= 0 && dp[start] && s.substring(start, i).equals(w)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];        
+    }
+}
