158aa3c65bd92b35a3930876ad95ccf34c3b202c
Lana Harriet <lanaharriet1408@gmail.com>
2026-07-25 09:15:49 +0530

    Create 1541-TopTravellers

---
+++ 1541-TopTravellers/1541-TopTravellers.sql
@@ -0,0 +1,12 @@
+-- Last updated: 7/25/2026, 9:15:49 AM
+# Write your MySQL query statement below
+SELECT DISTINCT
+    u.name,
+    IFNULL(
+        SUM(t.distance) OVER (PARTITION BY u.id),
+        0
+    ) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides t
+    ON u.id = t.user_id
+ORDER BY travelled_distance DESC, u.name ASC;
