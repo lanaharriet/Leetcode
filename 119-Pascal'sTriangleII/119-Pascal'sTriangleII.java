408721494c8fd938e84d21f595e81c23a1d9b16b
Lana Harriet <lanaharriet1408@gmail.com>
2026-08-06 09:19:27 +0530

    File updated at 8/6/2026, 9:19:26 AM

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,10 +1,14 @@
-// Last updated: 8/6/2026, 9:18:28 AM
+// Last updated: 8/6/2026, 9:19:26 AM
 1class Solution {
-2    public int singleNumber(int[] nums) {
-3        int result=0;
-4        for(int x:nums){
-5            result^=x;
-6        }
-7        return result;
-8    }
-9}
+2    public String convertToTitle(int columnNumber) {
+3        StringBuilder res = new StringBuilder();
+4
+5        while (columnNumber > 0) {
+6            columnNumber--;
+7            res.insert(0, (char) ((columnNumber % 26) + 'A'));
+8            columnNumber /= 26;
+9        }
+10        
+11        return res.toString();        
+12    }
+13}
