6273f1edb2eda143101a19459d1b51ccdcfb0d3f
Lana Harriet <lanaharriet1408@gmail.com>
2026-08-06 09:12:38 +0530

    File updated at 8/6/2026, 9:12:37 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.py
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.py
@@ -1,7 +1,19 @@
-# Last updated: 8/6/2026, 9:11:22 AM
+# Last updated: 8/6/2026, 9:12:37 AM
 1class Solution:
-2    def strStr(self, haystack, needle):
-3        for i in range(len(haystack) - len(needle) + 1):
-4            if haystack[i:i+len(needle)] == needle:
-5                return i
-6        return -1
+2  def addBinary(self, a: str, b: str) -> str:
+3    s = []
+4    carry = 0
+5    i = len(a) - 1
+6    j = len(b) - 1
+7
+8    while i >= 0 or j >= 0 or carry:
+9      if i >= 0:
+10        carry += int(a[i])
+11        i -= 1
+12      if j >= 0:
+13        carry += int(b[j])
+14        j -= 1
+15      s.append(str(carry % 2))
+16      carry //= 2
+17
+18    return ''.join(reversed(s))
