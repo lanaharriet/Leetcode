a5ba7518199c5f5d96162ed9e6cfdcee38b13b1d
Lana Harriet <lanaharriet1408@gmail.com>
2026-08-06 09:16:52 +0530

    File updated at 8/6/2026, 9:16:51 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,25 +1,14 @@
-// Last updated: 8/6/2026, 9:15:59 AM
-1class Solution {
-2    public List<List<Integer>> generate(int numRows) {
-3        if (numRows == 0) return new ArrayList<>();
-4        if (numRows == 1) {
-5            List<List<Integer>> result = new ArrayList<>();
-6            result.add(Arrays.asList(1));
-7            return result;
-8        }
-9        
-10        List<List<Integer>> prevRows = generate(numRows - 1);
-11        List<Integer> newRow = new ArrayList<>();
-12        
-13        for (int i = 0; i < numRows; i++) {
-14            newRow.add(1);
-15        }
-16        
-17        for (int i = 1; i < numRows - 1; i++) {
-18            newRow.set(i, prevRows.get(numRows - 2).get(i - 1) + prevRows.get(numRows - 2).get(i));
-19        }
-20        
-21        prevRows.add(newRow);
-22        return prevRows;
-23    }
-24}
+// Last updated: 8/6/2026, 9:16:51 AM
+1public class Solution {
+2    public List<Integer> getRow(int rowIndex) {
+3        List<Integer> res = new ArrayList<>();
+4        res.add(1);
+5        long prev = 1;
+6        for (int k = 1; k <= rowIndex; k++) {
+7            long next_val = prev * (rowIndex - k + 1) / k;
+8            res.add((int) next_val);
+9            prev = next_val;
+10        }
+11        return res;
+12    }
+13}
