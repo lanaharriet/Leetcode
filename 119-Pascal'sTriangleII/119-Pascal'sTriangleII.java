c7af1e66121db01c67d9f0a62dbb2b7043827063
Lana Harriet <lanaharriet1408@gmail.com>
2026-08-06 09:21:45 +0530

    File updated at 8/6/2026, 9:21:44 AM

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,14 +1,25 @@
-// Last updated: 8/6/2026, 9:19:26 AM
+// Last updated: 8/6/2026, 9:21:44 AM
 1class Solution {
-2    public String convertToTitle(int columnNumber) {
-3        StringBuilder res = new StringBuilder();
-4
-5        while (columnNumber > 0) {
-6            columnNumber--;
-7            res.insert(0, (char) ((columnNumber % 26) + 'A'));
-8            columnNumber /= 26;
-9        }
-10        
-11        return res.toString();        
-12    }
-13}
+2    public int majorityElement(int[] nums) {
+3
+4        int n = nums.length;
+5
+6        for(int i = 0; i < n; i++){
+7
+8            int count = 0;
+9
+10            for(int j = 0; j < n; j++){
+11
+12                if(nums[i] == nums[j]){
+13                    count++;
+14                }
+15            }
+16
+17            if(count > n / 2){
+18                return nums[i];
+19            }
+20        }
+21
+22        return -1;
+23    }
+24}
