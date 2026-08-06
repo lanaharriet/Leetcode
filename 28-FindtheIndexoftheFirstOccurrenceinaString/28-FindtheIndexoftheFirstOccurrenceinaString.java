e2b7e0612642781d3ec36f00d44d07cadf92b87a
Lana Harriet <lanaharriet1408@gmail.com>
2026-08-06 09:15:19 +0530

    File updated at 8/6/2026, 9:15:18 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,21 +1,22 @@
-// Last updated: 8/6/2026, 9:14:45 AM
+// Last updated: 8/6/2026, 9:15:18 AM
 1class Solution {
-2    public TreeNode sortedArrayToBST(int[] nums) {
-3        return convert(nums, 0, nums.length - 1);        
+2    public boolean isBalanced(TreeNode root) {
+3        return dfsHeight(root) != -1;
 4    }
 5
-6    private TreeNode convert(int[] nums, int left, int right) {
-7        if (left > right) {
-8            return null;
-9        }
-10        
-11        int mid = left + (right - left) / 2;
-12        
-13        TreeNode node = new TreeNode(nums[mid]);
-14        
-15        node.left = convert(nums, left, mid - 1);
-16        node.right = convert(nums, mid + 1, right);
-17        
-18        return node;
-19    }    
-20}
+6    private int dfsHeight(TreeNode node) {
+7        if (node == null) return 0;
+8
+9        int leftHeight = dfsHeight(node.left);
+10        if (leftHeight == -1) return -1;
+11
+12        int rightHeight = dfsHeight(node.right);
+13        if (rightHeight == -1) return -1;
+14
+15        if (Math.abs(leftHeight - rightHeight) > 1) {
+16            return -1;
+17        }
+18
+19        return 1 + Math.max(leftHeight, rightHeight);
+20    }
+21}
