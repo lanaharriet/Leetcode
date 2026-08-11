133628e21984f9ae729a7dfefda2f5f98d57a20e
Lana Harriet <lanaharriet1408@gmail.com>
2026-08-11 09:20:09 +0530

    File updated at 8/11/2026, 9:20:06 AM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.java
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.java
@@ -1,27 +1,16 @@
-// Last updated: 8/11/2026, 9:17:46 AM
+// Last updated: 8/11/2026, 9:20:06 AM
 1class Solution {
-2    public int[] intersect(int[] nums1, int[] nums2) {
-3        int l1 = nums1.length;
-4        int l2 = nums2.length;
-5        int i = 0, j = 0, k = 0;
-6        Arrays.sort(nums1);
-7        Arrays.sort(nums2);
-8        while( i < l1 && j < l2)
-9        {
-10            if(nums1[i] < nums2[j])
-11            {
-12                i++;
-13            }
-14            else if(nums1[i] > nums2[j])
-15            {
-16                j++;
-17            }
-18            else
-19            {
-20                nums1[k++] = nums1[i++];
-21                j++;
-22            }
-23        }
-24        return Arrays.copyOfRange(nums1,0,k);
-25    }
-26}
+2    public boolean isSubsequence(String s, String t) {
+3        int sp = 0;
+4        int tp = 0;
+5
+6        while (sp < s.length() && tp < t.length()) {
+7            if (s.charAt(sp) == t.charAt(tp)) {
+8                sp++;
+9            }
+10            tp++;
+11        }
+12
+13        return sp == s.length();        
+14    }
+15}
