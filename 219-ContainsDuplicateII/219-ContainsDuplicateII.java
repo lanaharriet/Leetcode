bb6f32a6574a8c36508de35f08996e3b8ad73c4a
Lana Harriet <lanaharriet1408@gmail.com>
2026-08-11 09:16:37 +0530

    File updated at 8/11/2026, 9:16:34 AM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.java
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.java
@@ -1,15 +1,15 @@
-// Last updated: 8/11/2026, 9:01:37 AM
-1class Solution {
-2    public boolean isUgly(int n) {
-3        if (n <= 0) {
-4            return false;
-5        }
-6        int[] factors = {2, 3, 5};
-7        for (int factor : factors) {
-8            while (n % factor == 0) {
-9                n /= factor;
-10            }
-11        }
-12        return n == 1;
+// Last updated: 8/11/2026, 9:16:34 AM
+1class NumArray {
+2    private int[] prefix;
+3
+4    public NumArray(int[] nums) {
+5        prefix = new int[nums.length + 1];
+6        for (int i = 0; i < nums.length; i++) {
+7            prefix[i + 1] = prefix[i] + nums[i];
+8        }
+9    }
+10
+11    public int sumRange(int left, int right) {
+12        return prefix[right + 1] - prefix[left];
 13    }
 14}
