f91ffd89040a0924103188587029cff201599200
Lana Harriet <lanaharriet1408@gmail.com>
2026-08-11 09:21:43 +0530

    File updated at 8/11/2026, 9:21:40 AM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.java
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.java
@@ -1,16 +1,14 @@
-// Last updated: 8/11/2026, 9:20:06 AM
+// Last updated: 8/11/2026, 9:21:40 AM
 1class Solution {
-2    public boolean isSubsequence(String s, String t) {
-3        int sp = 0;
-4        int tp = 0;
-5
-6        while (sp < s.length() && tp < t.length()) {
-7            if (s.charAt(sp) == t.charAt(tp)) {
-8                sp++;
+2    public List<String> readBinaryWatch(int turnedOn) {
+3        List<String> result = new ArrayList<>();
+4        for (int h = 0; h < 12; h++) {
+5            for (int m = 0; m < 60; m++) {
+6                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+7                    result.add(String.format("%d:%02d", h, m));
+8                }
 9            }
-10            tp++;
-11        }
-12
-13        return sp == s.length();        
-14    }
-15}
+10        }
+11        return result;
+12    }
+13}
