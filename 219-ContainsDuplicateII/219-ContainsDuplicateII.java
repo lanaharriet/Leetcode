ab68b5e3611d3a46a3c9ab852b16412ff509cc81
Lana Harriet <lanaharriet1408@gmail.com>
2026-08-11 09:17:49 +0530

    File updated at 8/11/2026, 9:17:46 AM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.java
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.java
@@ -1,15 +1,27 @@
-// Last updated: 8/11/2026, 9:16:34 AM
-1class NumArray {
-2    private int[] prefix;
-3
-4    public NumArray(int[] nums) {
-5        prefix = new int[nums.length + 1];
-6        for (int i = 0; i < nums.length; i++) {
-7            prefix[i + 1] = prefix[i] + nums[i];
-8        }
-9    }
-10
-11    public int sumRange(int left, int right) {
-12        return prefix[right + 1] - prefix[left];
-13    }
-14}
+// Last updated: 8/11/2026, 9:17:46 AM
+1class Solution {
+2    public int[] intersect(int[] nums1, int[] nums2) {
+3        int l1 = nums1.length;
+4        int l2 = nums2.length;
+5        int i = 0, j = 0, k = 0;
+6        Arrays.sort(nums1);
+7        Arrays.sort(nums2);
+8        while( i < l1 && j < l2)
+9        {
+10            if(nums1[i] < nums2[j])
+11            {
+12                i++;
+13            }
+14            else if(nums1[i] > nums2[j])
+15            {
+16                j++;
+17            }
+18            else
+19            {
+20                nums1[k++] = nums1[i++];
+21                j++;
+22            }
+23        }
+24        return Arrays.copyOfRange(nums1,0,k);
+25    }
+26}
